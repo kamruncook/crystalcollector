4893536c0df5893fe25d6a2171b7390b80697661
Cook Family <Cook Family>
2017-04-15 21:10:29 -0700

    crystalcollector

---
+++ assets/javascript/game.js
@@ -0,0 +1,36 @@
+var total = 0;
+var wins = 0;
+var losses = 0;
+
+$("Wins").text(wins);
+$("Losses").text(losses);
+
+function reset () {
+}
+
+function win () {
+  alert("Win!");
+  wins++;
+}
+
+function lose () {
+  alert("Lose!");
+  losses++;
+}
+
+$ function("#red").on ("click") {
+
+}
+
+$ function("#blue").on(click) {
+
+}
+
+$ function("#green").on(click) {
+
+}
+
+$ function("#yellow").on(click) {
+
+}
+
